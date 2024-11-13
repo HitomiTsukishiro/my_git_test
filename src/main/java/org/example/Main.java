f20652fd3e4796f03b14fbd19fb3ccdebaea1932
xiaowang <MyEmail>
2024-11-13 21:51:44 +0800

    第二次测试

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,8 @@ public class Main {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修正。
         System.out.printf("Hello and welcome!");
+        System.out.printf("1111111111111111111!");
+        System.out.printf("22222222222222222222222222!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
